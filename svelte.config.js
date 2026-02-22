53b0f8ec1e352e9bf1e3400b8c9bdd01814ed394
Kirill Sedunov <kirill.sedunov@outlook.com>
2026-02-22 22:43:56 +0300

    ci: add github action to release to pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+
+let adapter = adapterAuto();
+
+if (process.env.BUILD_TARGET === 'github-pages') {
+	const { default: adapterStatic } = await import('@sveltejs/adapter-static');
+	adapter = adapterStatic({
+		fallback: '404.html'
+	});
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter
 	}
 };
 
