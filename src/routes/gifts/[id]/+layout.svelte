3fcbe766b40d879c744c1ae98d4916ee6a105263
Kirill Sedunov <kirill.sedunov@outlook.com>
2026-02-22 20:06:09 +0300

    fix: remove old dependencies from layout

--- src/routes/gifts/[id]/+layout.svelte
+++ src/routes/gifts/[id]/+layout.svelte
@@ -1,8 +1,4 @@
 <script>
-	import AppHeader from '$lib/components/AppHeader.svelte';
-	import Button from '$lib/components/Button.svelte';
-	import { m } from '$lib/paraglide/messages';
-
 	let { children } = $props();
 </script>
 
